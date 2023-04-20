7cd0022d989143c0b3101f0fe53e010c274f1d07
hckim96 <hyeoncheolkim96@gmail.com>
2023-04-21 03:30:50 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/com/swm47/swminder/SwminderApplication.java
@@ -0,0 +1,13 @@
+package com.swm47.swminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwminderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwminderApplication.class, args);
+    }
+
+}
